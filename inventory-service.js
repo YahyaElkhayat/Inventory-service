4263c13ead10089b87ba6182313c025cc09faca3
Yahya elkhayat <yahyaelkhayat6@gmail.com>
2025-03-17 12:19:44 +0000

    update3

--- inventory-service.js
+++ inventory-service.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 //hello guys how are youuuu
 // Mock Inventory hhhh
+//yes buddy
 let inventory = {
   'item1': { name: 'Item 1', quantity: 10 },
   'item2': { name: 'Item 2', quantity: 5 }
